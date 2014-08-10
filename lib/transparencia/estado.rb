6d9ef7d8e38a8b5722e516957a07cd48f70f6c3b
cristianounix <cristianounix@gmail.com>
2014-08-10 20:11:12 -0300

    Lib Estado

--- lib/transparencia/estado.rb
+++ lib/transparencia/estado.rb
@@ -1,16 +1,10 @@
-#require 'ostruct'
-require 'httparty'
-require 'json'
-
 module Transparencia
-  class Estado
-    include HTTParty
-    base_uri Settings.transparencia.host
+  class Estado < Transparencia::Base
     ENTRY_POINT = Settings.transparencia.services.estados
 
-    def self.find(token)
-      attributes = JSON.parse(get(ENTRY_POINT + token ).body)['school']
-      new attributes
+    def self.all
+      estados = JSON.parse(get(ENTRY_POINT).body)
+      estados
     end
 
   end
