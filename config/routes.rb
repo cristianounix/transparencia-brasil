1bb13d941f986fa636161c97f232be7075cc177f
cristianounix <cristianounix@gmail.com>
2014-08-10 20:10:48 -0300

    Rota dos recursos Estado e Partido

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -25,6 +25,9 @@ Rails.application.routes.draw do
   #       get 'sold'
   #     end
   #   end
+
+  resource :estados, only: [:show]
+  resource :partidos, only: [:show]
 
   # Example resource route with sub-resources:
   #   resources :products do
