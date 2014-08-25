25e3708b72ff14b9dd4562c4928d70aec2827330
cristianounix <cristianounix@gmail.com>
2014-08-24 23:42:56 -0300

    Movendo pasta a para dentro da public

--- public/a/Gruntfile.js
+++ public/a/Gruntfile.js
@@ -66,9 +66,9 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9000,
+        port: 9922,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: '192.168.0.17',
+        hostname: 'localhost',
         livereload: 35729
       },
       livereload: {
