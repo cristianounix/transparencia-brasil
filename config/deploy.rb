14ec5f053d3f2a1c779fc7d8a013fd1b5bcb6ba0
cristianounix <cristianounix@gmail.com>
2014-08-28 21:42:21 -0300

    Fix file deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,14 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-<<<<<<< HEAD
-=======
 set :application, 'DeCaraLimpa'
 set :repo_url, 'git@github.com:cristianounix/transparencia-brasil.git'
->>>>>>> development
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-<<<<<<< HEAD
-=======
 set :deploy_to, '~/decaralimpa'
->>>>>>> development
 # set :deploy_to, '/var/www/my_app'
 
 # Default value for :scm is :git
