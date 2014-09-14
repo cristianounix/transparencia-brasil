24d8e493b4046b90f01bae160d1741d8f59560ad
Rodnei A. Ribeiro <rodnei.solucoes@gmail.com>
2014-09-14 18:34:17 -0300

    Adicionando paleta de cores aos cards dos candidatos e estado

--- front/app/scripts/animation/animation.js
+++ front/app/scripts/animation/animation.js
@@ -1,5 +1,16 @@
 'use strict';
 
 (function(){
+    jQuery(document).on('click', '.select span, .select i', function(){
+        jQuery('.select-box').slideToggle(50);
+    });
+
+    var elements = document.getElementsByClassName('block');
+    var colors = chroma.scale('Set1').domain([0,1], elements.length).colors();
+
+
+    for(var i = 0; i < elements.length; i++){
+        elements[i].style.backgroundColor = colors[i];
+    }
 
 }(jQuery));
