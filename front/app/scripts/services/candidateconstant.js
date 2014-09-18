5d42f82741151812d616dc5160883eb37a9b9fa8
Rodnei A. Ribeiro <rodnei.solucoes@gmail.com>
2014-09-17 22:52:08 -0300

    Adicionando constant e controller padrão para candidatos

---
+++ front/app/scripts/services/candidateconstant.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name frontApp.candidateConstant
+ * @description
+ * # candidateConstant
+ * Constant in the frontApp.
+ */
+angular.module('frontApp')
+  .constant('candidateConstant', {
+
+  });
