b24f5b83e9c46fcf6d60ab3a8caa554bac4d71da
cristianounix <cristianounix@gmail.com>
2014-09-24 01:24:27 -0300

    Criando view com Jbuilder e fazendo merge de dados do Google com a API oficial

--- app/controllers/estados_controller.rb
+++ app/controllers/estados_controller.rb
@@ -2,7 +2,11 @@ class EstadosController < ApplicationController
 	respond_to :json
 
 	def show
+    if params[:cargo] == 3
+      @vet_count = Transparencia::Google.count_senador
+    else
+      @vet_count = Transparencia::Google.count_senador
+    end
 		@estados = Transparencia::Estado.all
-		respond_with @estados
 	end
 end
