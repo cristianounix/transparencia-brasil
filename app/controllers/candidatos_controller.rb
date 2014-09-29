55afd40c219622f6ae7f8ee00de61711a591f1bb
cristianounix <cristianounix@gmail.com>
2014-09-28 21:04:10 -0300

    Cargo na api

--- app/controllers/candidatos_controller.rb
+++ app/controllers/candidatos_controller.rb
@@ -2,7 +2,7 @@ class CandidatosController < ApplicationController
 	respond_to :json
 
 	def show
-		q = URI.encode(params.slice(:estado,:partido,:limit).to_query)
+		q = URI.encode(params.slice(:estado,:cargo).to_query)
 		@excelencias = Transparencia::Candidato.find_all(q)
 		respond_with @excelencias
 	end
